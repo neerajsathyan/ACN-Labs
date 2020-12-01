f99a12e41b3f131e878829236fdcd1b8cf34cdea
m1das13 <midas.amersfoort@gmail.com>
2020-12-01 21:29:34 +0100

    added mac_to_port functionality

--- lab3/sp_routing.py
+++ lab3/sp_routing.py
@@ -30,7 +30,7 @@ from ryu.lib.packet import arp
 
 
 ###################################
-from ryu.lib.packet import ethernet
+from ryu.lib.packet import ethernet, ether_types
 from ryu.lib.packet import icmp
 ###################################
 
@@ -40,6 +40,10 @@ from ryu.app.wsgi import ControllerBase
 
 import topo
 import dijkstra
+from collections import defaultdict
+
+def default_dict():
+	return defaultdict(default_dict)
 
 
 class SPRouter(app_manager.RyuApp):
@@ -50,15 +54,11 @@ class SPRouter(app_manager.RyuApp):
         super(SPRouter, self).__init__(*args, **kwargs)
         self.topo_net = topo.Fattree(4)
 
-        #################################################################
-        # hardcoded hw_addr and ip_addr, as proposed by some other 
-        # architecture (not sure about this!) 
-        self.hw_addr = '0a:e4:1c:d1:3e:44'
-        self.ip_addr = '192.0.2.9'
-        #################################################################
-
-        # USed for learning switch functioning
+        # Used for learning switch functioning
         self.mac_to_port = {}
+
+        self.shortest_path_dict = default_dict()
+
         
         # calculate shortest paths between all server pairs
         self.servers = self.topo_net.servers
@@ -78,17 +78,17 @@ class SPRouter(app_manager.RyuApp):
 
         # for s in link_list:
 
-            # print (" \t\t s: " + str(s))
-            # print(" \t\t s.src.name: " + str(s.src.name))
-            # print(" \t\t s.src.name.split(-): " + str(s.src.name).split("-")[0][2:])
-            # print(" \t\t s.src.port_no: " + str(s.src.port_no))
-            # print(" \t\t s.src.hw_addr: " + str(s.src.hw_addr))
+        #     print (" \t\t s: " + str(s))
+        #     print(" \t\t s.src.name: " + str(s.src.name))
+        #     print(" \t\t s.src.name.split(-): " + str(s.src.name).split("-")[0][2:])
+        #     print(" \t\t s.src.port_no: " + str(s.src.port_no))
+        #     print(" \t\t s.src.hw_addr: " + str(s.src.hw_addr))
 
-            # print(" \t\t s.dst.name: " + str(s.dst.name))
-            # print(" \t\t s.dst.name.split(-): " + str(s.dst.name).split("-")[0][2:])
-            # print(" \t\t s.dst.port_no: " + str(s.dst.port_no))
-            # print(" \t\t s.dst.hw_addr: " + str(s.dst.hw_addr))
-            # print()
+        #     print(" \t\t s.dst.name: " + str(s.dst.name))
+        #     print(" \t\t s.dst.name.split(-): " + str(s.dst.name).split("-")[0][2:])
+        #     print(" \t\t s.dst.port_no: " + str(s.dst.port_no))
+        #     print(" \t\t s.dst.hw_addr: " + str(s.dst.hw_addr))
+        #     print()
 
 
         #################################################################
@@ -97,12 +97,20 @@ class SPRouter(app_manager.RyuApp):
         # get list of dpid's of all switches
         self.switch_dpids = [switch.dp.id for switch in switch_list]
 
-        # link dictionary {src_dpid : {dst_dpid : {src_port_to_dst}}}
-        self.switch_dpid_links = {link.src.dpid : {link.dst.dpid : {'port': link.src.port_no}} for link in link_list}
 
-        print("Switches:\n", self.switch_dpids)
-        print("Links: \n", self.switch_dpid_links)
-        print()
+        self.switch_name_to_dpid = {str(link.src.name).split("-")[0][2:] : link.src.dpid for link in link_list} 
+        # print(self.switch_name_to_dpid)
+
+
+
+        # link dictionary {src_dpid : {dst_dpid : {src_port_to_dst}}}
+        self.switch_dpid_links = {link.src.dpid : {link.dst.dpid : {link.src.port_no}} for link in link_list}
+        # print("Dpid_links: \n", self.switch_dpid_links)
+
+        self.dpid_to_hw_addr = {link.src.dpid : link.src.hw_addr for link in link_list}
+        # print("Hw_addr_links: \n", self.dpid_to_hw_addr)
+        
+
 
         #################################################################
 
@@ -142,11 +150,7 @@ class SPRouter(app_manager.RyuApp):
 
         # TODO: handle new packets at the controller
 
-        # Calculate the path from server 'start' to server 'end'
-        start_id, end_id = 1, 14
-        shortest_path = dijkstra.shortest_path_list(self.dijkstra_table, start_id, end_id, self.n_servers)
-        print(f'Shortest_path = {shortest_path}\n')
-
+        #################################################################
 
         # record in_port
         in_port = msg.match['in_port']
@@ -154,46 +158,38 @@ class SPRouter(app_manager.RyuApp):
         pkt = packet.Packet(msg.data)
         pkt_eth = pkt.get_protocol(ethernet.ethernet)
 
-        if not pkt_eth:
+        # ignore lldp packet
+        if pkt_eth.ethertype == ether_types.ETH_TYPE_LLDP:
             return
-
-        #################################################################
-        # HANDLE ARP REQUESTS, ICMP AND IPV4 
-
-        pkt_arp = pkt.get_protocol(arp.arp)
-        pkt_icmp = pkt.get_protocol(icmp.icmp)
-        pkt_ipv4 = pkt.get_protocol(ipv4.ipv4)
-
-
-        if pkt_arp:
-            # print("datapath id: "+str(dpid))
-            # print("port: "+str(in_port))
-            # print("pkt_eth.dst: " + str(pkt_eth.dst))
-            # print("pkt_eth.src: " + str(pkt_eth.src))
-            # print("pkt_arp: " + str(pkt_arp))
-            # print("pkt_arp:src_ip: " + str(pkt_arp.src_ip))
-            # print("pkt_arp:dst_ip: " + str(pkt_arp.dst_ip))
-            # print("pkt_arp:src_mac: " + str(pkt_arp.src_mac))
-            # print("pkt_arp:dst_mac: " + str(pkt_arp.dst_mac))
-            # print()
-
-            self._handle_arp(datapath, in_port, pkt_eth, pkt_arp)
-            return
-
-        elif pkt_icmp:
-            self._handle_icmp(datapath, in_port, pkt_eth, pkt_ipv4, pkt_icmp)
-            return
-
-        #################################################################
-
-
-        #################################################################
-        # COPIED FROM LEARNING_SWITCH.PY (LAB1)
         
         dst = pkt_eth.dst
         src = pkt_eth.src
 
+        
+        #################################################################
+        src, dst = '000000002', '000000013'
+        #################################################################
+        # print(src, dst)
+
+        # calculate shortest path between src and dst if unknown
+        if not (src, dst) in self.shortest_path_dict.keys():
+            
+            shortest_path = self.calculate_shortest_path(src, dst)
+
+            # save shortest path for src and dst
+            self.shortest_path_dict[(src, dst)] = shortest_path
+
+            print(f'\n\nDpid shortest path = {self.shortest_path_dict[(src, dst)]}\n')
+
+            hw_addr_shortest_path = [self.dpid_to_hw_addr[int(dpid)] for dpid in self.shortest_path_dict[(src, dst)][1:-1] if int(dpid) in self.dpid_to_hw_addr]
+            hw_addr_shortest_path.insert(0, src)
+            hw_addr_shortest_path.insert(len(self.shortest_path_dict[(src, dst)]) + 1, dst)
+
+            print(f'Hw_addr_shortest_path = {hw_addr_shortest_path}\n')
+
+        # current dpid
         dpid = datapath.id
+
         self.mac_to_port.setdefault(dpid, {})
 
         # self.logger.info("\tpacket in %s %s %s %s", dpid, src, dst, in_port)
@@ -202,7 +198,18 @@ class SPRouter(app_manager.RyuApp):
         self.mac_to_port[dpid][src] = in_port
 
         if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
+
+            # determine next dpid hop for current dpid
+            next_dpid = self.shortest_path_dict[(src, dst)][dpid]
+
+            # translate next dpid to a hw_addr
+            next_dst = self.dpid_to_hw_addr[int(next_dpid)]
+
+            # set next hw_addr as out_port
+            out_port = self.mac_to_port[dpid][next_dst]
+            print(out_port)
+
+
         else:
             out_port = ofproto.OFPP_FLOOD
 
@@ -231,75 +238,49 @@ class SPRouter(app_manager.RyuApp):
         #################################################################
 
 
+    def calculate_shortest_path(self, src_mac, dst_mac):
 
-    #################################################################
-    # ADDED FUNCTIONS TO HANDLE ARP REQUESTS AND ICMP
+        # print(f'\nsrc_mac, dst_mac = {src_mac}, {dst_mac}')
+        src_server, dst_server = int(src_mac.replace('0', '').replace(':', '')), int(dst_mac.replace('0', '').replace(':', ''))
+        # print(f'src_server, dst_server = {src_server}, {dst_server}\n')
+        
 
-    def _handle_arp(self, datapath, port, pkt_ethernet, pkt_arp):
+        shortest_path_switches = dijkstra.shortest_path_list(self.dijkstra_table, src_server, dst_server, self.n_servers)
+        # print(f'Shortest_path = {shortest_path_switches}\n')
 
-        if not pkt_arp.opcode == arp.ARP_REQUEST:
-            return
+        shortest_path_mininet = []
 
-        pkt = packet.Packet()
+        for (type, id) in shortest_path_switches:
 
-        pkt.add_protocol(ethernet.ethernet(ethertype=pkt_ethernet.ethertype,
-                                        dst=pkt_ethernet.src,
-                                        src=self.hw_addr))
+            if type == 'server':
+                continue
+                # new_type = 'host_' + str(id)
+            elif type == 'edge switch':
+                new_type = 'es_' + str(id)
+            elif type == 'aggregate switch':
+                new_type = 'as_' + str(id)
+            elif type == 'core switch':
+                new_type = 'cs_' + str(id)
 
-        pkt.add_protocol(arp.arp(opcode=arp.ARP_REPLY,
-                                src_mac=self.hw_addr,
-                                src_ip=self.ip_addr,
-                                dst_mac=pkt_arp.src_mac,
-                                dst_ip=pkt_arp.src_ip))
+            shortest_path_mininet.append(new_type)
 
-        self._send_packet(datapath, port, pkt)
+        # print(f'Mininet_shortest_path = {shortest_path_mininet}\n')
+        
+        dpid_shortest_path = [self.switch_name_to_dpid[name] for name in shortest_path_mininet if name in self.switch_name_to_dpid]
+        dpid_shortest_path.insert(0, src_mac)
+        dpid_shortest_path.insert(len(dpid_shortest_path), dst_mac)
+
+        # print(f'Dpid_shortest_path = {dpid_shortest_path}\n')
+
+
+        # hw_addr_shortest_path = [self.dpid_to_hw_addr[int(dpid)] for dpid in dpid_shortest_path if int(dpid) in self.dpid_to_hw_addr]
+        # hw_addr_shortest_path.insert(0, src_mac)
+        # hw_addr_shortest_path.insert(len(dpid_shortest_path), dst_mac)
+
+        # print(f'Hw_addr_shortest_path = {hw_addr_shortest_path}\n')
 
     
-
-    def _handle_icmp(self, datapath, port, pkt_ethernet, pkt_ipv4, pkt_icmp):
-
-        if not pkt_icmp.type == icmp.ICMP_ECHO_REQUEST:
-            return
-
-        pkt = packet.Packet()
-
-        pkt.add_protocol(ethernet.ethernet(ethertype=pkt_ethernet.ethertype,
-                                           dst=pkt_ethernet.src,
-                                           src=self.hw_addr))
-
-        pkt.add_protocol(ipv4.ipv4(dst=pkt_ipv4.src,
-                                   src=self.ip_addr,
-                                   proto=pkt_ipv4.proto))
-
-        pkt.add_protocol(icmp.icmp(type_=icmp.ICMP_ECHO_REPLY,
-                                   code=icmp.ICMP_ECHO_REPLY_CODE,
-                                   csum=0,
-                                   data=pkt_icmp.data))
-
-        self._send_packet(datapath, port, pkt)
-
-
-    def _send_packet(self, datapath, port, pkt):
-
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-        pkt.serialize()
-
-        self.logger.info("packet-out %s" % (pkt,))
-
-        data = pkt.data
-        actions = [parser.OFPActionOutput(port=port)]
-
-        out = parser.OFPPacketOut(datapath=datapath,
-                                  buffer_id=ofproto.OFP_NO_BUFFER,
-                                  in_port=ofproto.OFPP_CONTROLLER,
-                                  actions=actions,
-                                  data=data)
-
-        datapath.send_msg(out)
-
-
-    #################################################################
+        return dpid_shortest_path
 
 
 
